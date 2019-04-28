a8c0a954886f02a3571b3332ef45f32372fb8bff
Nikola <nikola.3in1@gmail.com>
2019-04-29 00:53:17 +0200

    creating components

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,32 +2,26 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>Moj Broj</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
+      <v-btn flat>
+        <span class="mr-2">Version 1.0</span>
       </v-btn>
     </v-toolbar>
-
     <v-content>
-      <HelloWorld/>
+      <SolverForm/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import SolverForm from './components/HelloWorld'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SolverForm
   },
   data () {
     return {
